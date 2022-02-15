95d372e17d0ea7b12b16c1ae24998b7cbb3f31d7
ckstmznf <ckstmznf@naver.com>
2022-02-15 18:58:29 +0900

    Chatting UI

--- app/src/main/java/com/edcan/howtosunrin/ui/savequestion/SaveQnARecyclerHolder.kt
+++ app/src/main/java/com/edcan/howtosunrin/ui/savequestion/SaveQnARecyclerHolder.kt
@@ -3,14 +3,14 @@ package com.edcan.howtosunrin.ui.savequestion
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.edcan.howtosunrin.databinding.LayoutQuestionBinding
+import com.edcan.howtosunrin.databinding.ItemSaveQuestionBinding
 import com.edcan.howtosunrin.utill.qna.Question
 
-class SaveQnARecyclerHolder(private val binding : LayoutQuestionBinding)
+class SaveQnARecyclerHolder(private val binding : ItemSaveQuestionBinding)
     : RecyclerView.ViewHolder(binding.root) {
 
     constructor(parent: ViewGroup) : this(
-        LayoutQuestionBinding.inflate(LayoutInflater.from(parent.context), parent ,false)
+        ItemSaveQuestionBinding.inflate(LayoutInflater.from(parent.context), parent ,false)
     )
 
     init {
