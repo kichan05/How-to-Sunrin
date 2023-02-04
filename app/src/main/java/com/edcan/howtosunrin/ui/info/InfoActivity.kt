ec0dbd4c2414a3198ce52ed0c13368461c525fb7
Heechan <ckstmznf@naver.com>
2023-02-04 15:53:44 +0900

    Info activity toolbar action

--- app/src/main/java/com/edcan/howtosunrin/ui/info/InfoActivity.kt
+++ app/src/main/java/com/edcan/howtosunrin/ui/info/InfoActivity.kt
@@ -17,24 +17,30 @@ class InfoActivity : BaseActivity<ActivityInfoBinding>(R.layout.activity_info) {
         super.onCreate(savedInstanceState)
 
         with(binding){
+            tbInfo.setNavigationOnClickListener{ finish() }
+
             btnInfoOpenSource.setOnClickListener {
                 val intent = Intent(Intent.ACTION_VIEW, Uri.parse(Url.openSource.url))
                 startActivity(intent)
+                overridePendingTransition(R.anim.anim_slide_from_right, R.anim.anim_hold)
             }
 
             btnInfoClub.setOnClickListener {
                 val intent = Intent(Intent.ACTION_VIEW, Uri.parse(Url.edcanWeb.url))
                 startActivity(intent)
+                overridePendingTransition(R.anim.anim_slide_from_right, R.anim.anim_hold)
             }
 
             btnInfoDeveloper.setOnClickListener {
                 val intent = Intent(Intent.ACTION_VIEW, Uri.parse(Url.developerGithub.url))
                 startActivity(intent)
+                overridePendingTransition(R.anim.anim_slide_from_right, R.anim.anim_hold)
             }
 
             btnInfoDonation.setOnClickListener {
                 val intent = Intent(Intent.ACTION_VIEW, Uri.parse(Url.developerDonation.url))
                 startActivity(intent)
+                overridePendingTransition(R.anim.anim_slide_from_right, R.anim.anim_hold)
             }
 
         }
